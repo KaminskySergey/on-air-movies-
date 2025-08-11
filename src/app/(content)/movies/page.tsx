55f16ffa07c3955cf63d5ba473d1920df2631e17
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2025-08-11 22:51:40 +0300

    1

--- src/app/(content)/movies/page.tsx
+++ src/app/(content)/movies/page.tsx
@@ -1,14 +1,17 @@
 "use server"
-import { redirect } from 'next/navigation'
+// import { redirect } from 'next/navigation'
 import { getTopTrending } from '../../../../actions/movies'
 
 export default async function MoviesPageRedirect() {
   const data = await getTopTrending({ type: 'movie' })
 
-  if (!data.results?.length) {
-    redirect('/')
-  }
+//   if (!data.results?.length) {
+//     redirect('/')
+//   }
 
-  const firstId = data.results[0].id
-  redirect(`/movies/${firstId}`)
+//   const firstId = data.results[0].id
+//   redirect(`/movies/${firstId}`)
+return <div>
+    {data.results[0].title}
+</div>
 }

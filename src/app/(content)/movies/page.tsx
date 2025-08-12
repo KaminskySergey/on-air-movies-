1df326b15c30afd0bcf4b5915556f95a5cab60b0
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2025-08-12 11:47:49 +0300

    13

--- src/app/(content)/movies/page.tsx
+++ src/app/(content)/movies/page.tsx
@@ -1,15 +1,19 @@
 "use server"
+import Link from 'next/link';
 import { getTopTrending } from '../../../../actions/movies'
 
 
 
 export default async function MoviesPage() {
-  const data = await getTopTrending({ type: "movie" });
+    const data = await getTopTrending({ type: "movie" });
 
-  return (
-    <div className='text-blue-600'>
-        {data.results[0].original_title}
-        dfvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-    </div>
-  )
+    return (
+        <div style={{ padding: 20, fontSize: 18 }}>
+            🎬 Movies list will be here soon!
+            <br />
+            <Link href={`/movies/${data.results[0].id}`}>
+                CLick me
+            </Link>
+        </div>
+    )
 }

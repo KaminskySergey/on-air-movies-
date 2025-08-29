25525816f4b526902f76c56c9a944dd8c3731717
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2025-08-29 22:57:58 +0300

    fix credits list height

--- src/components/kino/credits/credits-current-kino.tsx
+++ src/components/kino/credits/credits-current-kino.tsx
@@ -12,12 +12,14 @@ interface ICreditsCurrentKino {
 export function CreditsCurrentKino({ credits }: ICreditsCurrentKino) {
     const [activeTab, setActiveTab] = useState<string>("cast")
     const { cast, crew } = credits
-    
+
     return (
         <div className="flex flex-col gap-5 sm:gap-7">
-            <CreditsTabs  credits={credits} setActiveTab={setActiveTab} activeTab={activeTab}/>
-            {activeTab === "cast" && <CreditsList credits={cast} />}
-            {activeTab === "crew" && <CreditsList credits={crew} />}
+            <CreditsTabs credits={credits} setActiveTab={setActiveTab} activeTab={activeTab} />
+            <div className="min-h-[550px]">
+                {activeTab === "cast" && <CreditsList credits={cast} />}
+                {activeTab === "crew" && <CreditsList credits={crew} />}
+            </div>
         </div>
     );
 }
